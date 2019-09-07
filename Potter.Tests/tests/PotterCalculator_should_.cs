141877964a5aac80db8df1d13bde050f0b23695c
Sergio A <saguado78@gmail.com>
2019-09-07 13:39:40 +0200

    refactor. Create a function to generate test shopping carts

--- Potter.Tests/tests/PotterCalculator_should_.cs
+++ Potter.Tests/tests/PotterCalculator_should_.cs
@@ -6,6 +6,35 @@ namespace Potter.Tests.tests
     public class PotterCalculator_should_
     {
         const double defaultPrice=8.0;
+
+        private ShoppingCart BuildMeAShoppingCart(int amountBook1
+                        ,int amountBook2
+                        ,int amountBook3
+                        ,int amountBook4
+                        ,int amountBook5){
+            ShoppingCart res = new ShoppingCart();
+            while(amountBook1>0){
+                res.CartBooks.Add(new Book(1,defaultPrice));
+                amountBook1--;
+            }
+            while(amountBook2>0){
+                res.CartBooks.Add(new Book(2,defaultPrice));
+                amountBook2--;
+            }
+            while(amountBook3>0){
+                res.CartBooks.Add(new Book(3,defaultPrice));
+                amountBook3--;
+            }
+            while(amountBook4>0){
+                res.CartBooks.Add(new Book(4,defaultPrice));
+                amountBook4--;
+            }
+            while(amountBook5>0){
+                res.CartBooks.Add(new Book(5,defaultPrice));
+                amountBook5--;
+            }
+            return res;
+        }
         [Test]
         public void return_zero_when_buying_no_books(){
             PotterCalculator calc = new PotterCalculator();
@@ -18,8 +47,7 @@ namespace Potter.Tests.tests
         [Test]
         public void return_8_when_buying_a_single_book(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(1,0,0,0,0);
 
             double res= calc.Calculate(cart);
 
@@ -29,9 +57,7 @@ namespace Potter.Tests.tests
         [Test]
         public void return_16_when_buying_2_copies_of_a_book(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
-            cart.CartBooks.Add(new Book(1,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(2,0,0,0,0);
 
             double res= calc.Calculate(cart);
 
@@ -41,9 +67,7 @@ namespace Potter.Tests.tests
         [Test]
         public void apply_5_percent_discount_when_buying_1_copy_of_2_different_books(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
-            cart.CartBooks.Add(new Book(2,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(1,1,0,0,0);
 
             double res= calc.Calculate(cart);
 
@@ -53,10 +77,7 @@ namespace Potter.Tests.tests
         [Test]
         public void apply_10_percent_discount_when_buying_1_copy_of_3_different_books(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
-            cart.CartBooks.Add(new Book(2,defaultPrice));
-            cart.CartBooks.Add(new Book(3,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(1,1,1,0,0);
 
             double res= calc.Calculate(cart);
 
@@ -66,11 +87,7 @@ namespace Potter.Tests.tests
         [Test]
         public void apply_20_percent_discount_when_buying_1_copy_of_4_different_books(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
-            cart.CartBooks.Add(new Book(2,defaultPrice));
-            cart.CartBooks.Add(new Book(3,defaultPrice));
-            cart.CartBooks.Add(new Book(4,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(1,1,1,1,0);
             
             double res= calc.Calculate(cart);
 
@@ -80,12 +97,7 @@ namespace Potter.Tests.tests
         [Test]
         public void apply_25_percent_discount_when_buying_1_copy_of_5_different_books(){
             PotterCalculator calc = new PotterCalculator();
-            ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,defaultPrice));
-            cart.CartBooks.Add(new Book(2,defaultPrice));
-            cart.CartBooks.Add(new Book(3,defaultPrice));
-            cart.CartBooks.Add(new Book(4,defaultPrice));
-            cart.CartBooks.Add(new Book(5,defaultPrice));
+            ShoppingCart cart=BuildMeAShoppingCart(1,1,1,1,1);
             
             double res= calc.Calculate(cart);
 
