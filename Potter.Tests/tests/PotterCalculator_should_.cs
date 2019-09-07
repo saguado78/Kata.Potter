5de40c3bf762a517d1f66e353bc842fb09b1b2e8
Sergio A <saguado78@gmail.com>
2019-09-07 13:16:36 +0200

    Create tests constant for default price

--- Potter.Tests/tests/PotterCalculator_should_.cs
+++ Potter.Tests/tests/PotterCalculator_should_.cs
@@ -5,6 +5,7 @@ namespace Potter.Tests.tests
 [TestFixture]
     public class PotterCalculator_should_
     {
+        const double defaultPrice=8.0;
         [Test]
         public void return_zero_when_buying_no_books(){
             PotterCalculator calc = new PotterCalculator();
@@ -18,35 +19,35 @@ namespace Potter.Tests.tests
         public void return_8_when_buying_a_single_book(){
             PotterCalculator calc = new PotterCalculator();
             ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,8.0));
+            cart.CartBooks.Add(new Book(1,defaultPrice));
 
             double res= calc.Calculate(cart);
 
-            Assert.AreEqual(res,8.0,"unexpected result: " +res.ToString() + " // Expected 16");
+            Assert.AreEqual(res,defaultPrice,"unexpected result: " +res.ToString() + " // Expected 16");
         }
         
         [Test]
         public void return_16_when_buying_2_copies_of_a_book(){
             PotterCalculator calc = new PotterCalculator();
             ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,8.0));
-            cart.CartBooks.Add(new Book(1,8.0));
+            cart.CartBooks.Add(new Book(1,defaultPrice));
+            cart.CartBooks.Add(new Book(1,defaultPrice));
 
             double res= calc.Calculate(cart);
 
-            Assert.AreEqual(res,16.0,"unexpected result: " + res.ToString()+" // Expected 16");
+            Assert.AreEqual(res,2*defaultPrice,"unexpected result: " + res.ToString()+" // Expected 16");
         }
 
         [Test]
         public void apply_5_percent_discount_when_buying_1_copy_of_2_different_books(){
             PotterCalculator calc = new PotterCalculator();
             ShoppingCart cart=new ShoppingCart();
-            cart.CartBooks.Add(new Book(1,8.0));
-            cart.CartBooks.Add(new Book(2,8.0));
+            cart.CartBooks.Add(new Book(1,defaultPrice));
+            cart.CartBooks.Add(new Book(2,defaultPrice));
 
             double res= calc.Calculate(cart);
 
-            Assert.AreEqual(res,16.0*0.95,"unexpected result: " + res.ToString()+" // Expected 16");
+            Assert.AreEqual(res,2*defaultPrice*0.95,"unexpected result: " + res.ToString()+" // Expected 15.2");
         }
     }
 }
