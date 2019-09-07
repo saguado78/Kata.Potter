025581c472c0229d4e0fc9b63e57ecc15231879b
Sergio A <saguado78@gmail.com>
2019-09-07 11:32:08 +0200

    More clear messages when a test fails

--- Potter.Tests/tests/PotterCalculator_should_.cs
+++ Potter.Tests/tests/PotterCalculator_should_.cs
@@ -11,7 +11,7 @@ namespace Potter.Tests.tests
 
             double res= calc.Calculate(new ShoppingCart());
 
-            Assert.AreEqual(res,0,"unexpected result for empty shopping cart");
+            Assert.AreEqual(res,0,"unexpected result: " + res.ToString()+" // Expected 0");
         }
 
         [Test]
@@ -22,8 +22,19 @@ namespace Potter.Tests.tests
 
             double res= calc.Calculate(cart);
 
-            Assert.AreEqual(res,10.0,"unexpected result for empty shopping cart");
+            Assert.AreEqual(res,10.0,"unexpected result: " +res.ToString() + " // Expected 10");
         }
         
+                [Test]
+        public void return_20_when_buying_2_copies_of_a_book(){
+            PotterCalculator calc = new PotterCalculator();
+            ShoppingCart cart=new ShoppingCart();
+            cart.CartBooks.Add(new Book(1,10.0));
+            cart.CartBooks.Add(new Book(1,10.0));
+
+            double res= calc.Calculate(cart);
+
+            Assert.AreEqual(res,20.0,"unexpected result: " + res.ToString()+" // Expected 20");
+        }
     }
 }
